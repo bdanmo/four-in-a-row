14f460d9590377131bdec49c94e47a5ce2b96f0f
Brendan Moran <b.daniel.moran@gmail.com>
2020-07-30 13:36:51 -0400

    jsdoc for space and board, createSpaces method for board

--- js/Board.js
+++ js/Board.js
@@ -2,7 +2,23 @@ class Board {
   constructor() {
     this.rows = 6;
     this.columns = 7;
-    this.spaces = [];
+    this.spaces = this.createSpaces(this.columns, this.rows);
   }
-  createSpaces() {}
+
+  /**
+   *
+   * @param {number} x - number of columns/spaces on the x axis
+   * @param {number} y - number of rows/spaces on the y axis
+   * @returns {Array} spaces - an array of instances of the Space class
+   */
+  createSpaces(x, y) {
+    const spaces = [];
+    for (let i = 0; i < x; i++) {
+      for (let i = 0; i < y; i++) {
+        let space = new Space(x, y);
+        spaces.push(space);
+      }
+    }
+    return spaces;
+  }
 }
--- js/Space.js
+++ js/Space.js
@@ -1,7 +1,14 @@
+/** A class representing a space in the game board */
 class Space {
+  /**
+   * Creates a space
+   * @param {number} x
+   * @param {number} y
+   */
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    /** */
     this.id = `space-${x}-${y}`;
   }
 }
