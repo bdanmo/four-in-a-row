d5ef58a5c0de585a01659bec68c74aa0e8eb358c
Brendan Moran <b.daniel.moran@gmail.com>
2020-08-02 18:46:32 -0400

    Added createHTMLtoken method to Token class.

--- js/Token.js
+++ js/Token.js
@@ -5,4 +5,16 @@ class Token {
     this.color = this.owner.color;
     this.dropped = false;
   }
+
+  createHTMLtoken() {
+    const tokenDiv = document.createElement("div");
+    tokenDiv.setAttribute("id", this.id);
+    tokenDiv.setAttribute("class", "token");
+    tokenDiv.style.backgroundColor = this.color;
+    document.getElementById("game-board-underlay").appendChild(tokenDiv);
+  }
+
+  get htmlToken() {
+    return this.createHTMLtoken();
+  }
 }
