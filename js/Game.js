2e24833ec70ddacd3e04823472f69d8cc1533591
Brendan Moran <b.daniel.moran@gmail.com>
2020-08-11 20:20:58 -0400

    added switchPlayer and checkForWin methods

--- js/Game.js
+++ js/Game.js
@@ -55,6 +55,81 @@ class Game {
     }
   }
 
+  /** 
+   * Checks for a winning move
+   * @param {token} target - The last space that a token was dropped into
+   * @returns {boolean} Boolena indicating whether or not the last move was a winning move.
+   * @author Ashley Bouchet
+   */
+  checkForWin(target) {
+    const owner = target.token.owner;
+    let win = false;
+
+    // vertical
+    for (let x = 0; x < this.board.columns; x++) {
+      for (let y = 0; y < this.board.rows - 3; y++) {
+        if (this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x][y + 1].owner === owner &&
+          this.board.spaces[x][y + 2].owner === owner &&
+          this.board.spaces[x][y + 3].owner === owner) {
+          win = true;
+        }
+      }
+    }
+
+    // horizontal
+    for (let x = 0; x < this.board.columns - 3; x++) {
+      for (let y = 0; y < this.board.rows; y++) {
+        if (this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x + 1][y].owner === owner &&
+          this.board.spaces[x + 2][y].owner === owner &&
+          this.board.spaces[x + 3][y].owner === owner) {
+          win = true;
+        }
+      }
+    }
+
+    // diagonal
+    for (let x = 3; x < this.board.columns; x++) {
+      for (let y = 0; y < this.board.rows - 3; y++) {
+        if (this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x - 1][y + 1].owner === owner &&
+          this.board.spaces[x - 2][y + 2].owner === owner &&
+          this.board.spaces[x - 3][y + 3].owner === owner) {
+          win = true;
+        }
+      }
+    }
+
+    // diagonal
+    for (let x = 3; x < this.board.columns; x++) {
+      for (let y = 3; y < this.board.rows; y++) {
+        if (this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x - 1][y - 1].owner === owner &&
+          this.board.spaces[x - 2][y - 2].owner === owner &&
+          this.board.spaces[x - 3][y - 3].owner === owner) {
+          win = true;
+        }
+      }
+    }
+
+    return win;
+  }
+
+  /** 
+   * Switches active player. 
+   */
+
+  switchPlayer() {
+    for (let player of this.players) {
+      if (player.active === true) {
+        player.active === false;
+      } else if (player.active === false) {
+        player.active === true;
+      }
+    }
+  }
+
   /**
    * Branches code, depending on what key player presses
    * @param   {Object}    e - Keydown event object
